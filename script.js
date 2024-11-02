3da8ddc539466f9e44a6738bbc2b663b985ab2c7
Orpsemius <jvbteixeira@hotmail.com>
2024-11-02 21:44:04 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+function parseMenu(){
+    fetch("menu.xml").then(response =>{
+        return response.text();
+    }).then(xmlString => {
+        const xmlDocument = new DOMParser().parseFromString(xmlString, "text/xml");
+        const meals = xmlDocument.querySelectorAll("meal");
+        const coffeeAndChocolate = xmlDocument.querySelectorAll("variation");
+        const descriptionCoffeeAndChocolate = xmlDocument.querySelector("descriptionCoffeAndChocolate").textContent;
+        const others = xmlDocument.querySelectorAll("otherBeverage");
+
+        for (const meal of meals){
+            var mealName = meal.querySelector("name").textContent;
+            var mealPrice = meal.querySelector("price").textContent;
+            var mealDescription = meal.querySelector("description").textContent;
+            var mealImage = meal.querySelector("image").textContent;
+
+            document.getElementById("mealsMenu").innerHTML += `<div class="card" style="width: 18rem;">
+            <img src="./images/${mealImage}" class="card-img-top" alt="image of ${mealName}">
+            <div class="card-body">
+              <h5 class="card-title">${mealName}</h5>
+              <h6 class="card-title">${mealPrice}</h6>
+              <p class="card-text">${mealDescription}</p>
+            </div>
+          </div>`
+            
+        }
+
+        for (const coffee of coffeeAndChocolate){
+            var coffeeSize = coffee.querySelector("size").textContent;
+            var coffeePrice = coffee.querySelector("price").textContent
+            var coffeeImage = coffee.querySelector("image").textContent;
+
+            document.getElementById("coffeeMenu").innerHTML += `<div class="card" style="width: 18rem;">
+            <img src="images/${coffeeImage}" class="card-img-top" alt="image of coffe and hot chocolate ">
+            <div class="card-body">
+              <h5 class="card-title">${coffeeSize}</h5>
+              <p class="card-text">${coffeePrice}</p>
+            </div>
+          </div>`
+        }
+
+        for (const other of others){
+            var otherName = other.querySelector("name").textContent;
+            var otherPrice = other.querySelector("price").textContent;
+            var otherImage = other.querySelector("image").textContent;
+
+            document.getElementById("otherMenu").innerHTML += `<div class="card" style="width: 18rem;">
+            <img src="images/${otherImage}" class="card-img-top" alt="image of ${otherName}">
+            <div class="card-body">
+              <h5 class="card-title">${otherName}</h5>
+              <p class="card-text">${otherPrice}</p>
+            </div>
+          </div>`
+        }
+
+    })
+}
+
+function parseContact(){
+  fetch("branches.xml").then(response =>{
+    return response.text();
+  }).then(xmlString => {
+    const xmlDocument = new DOMParser().parseFromString(xmlString, "text/xml");
+
+    const branches = xmlDocument.querySelectorAll("branch");
+
+    for (const branch of branches){
+      const address = branch.querySelector("address").textContent;
+      const contact = branch.querySelector("contact").textContent
+      const hours = branch.querySelector("openingHours").textContent;
+      const link = branch.querySelector("link").textContent;
+
+      document.getElementById("contactCards").innerHTML += `<div class="card" style="width: 33rem;">
+            <div class="card-body">
+              <a href="${link}"><h3 class="card-title">${address}</h3></a>
+              <h5 class="card-text">${hours}</h5>
+              <h6 class="card-text">Contact number: ${contact}</h6>
+
+            </div>
+          </div>`
+        }
+
+    })   
+
+  }
+
+function buttonAlert(){
+  alert("Thank you for contacting Bryan's Cafe.");
+}
+
+function footerYear(){
+  const date = new Date();
+  var year = date.getFullYear();
+  document.getElementById("footer-row").innerHTML = `<p>@${year} Bryan’s Café,</p>`;
+
+}
